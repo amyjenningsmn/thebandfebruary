45bcd68651d5158384e7d86f004e7975ab0548e9
Amy Jennings <amyjenningsmn@gmail.com>
2015-12-08 16:33:51 -0600

    Added a gallery using Lightbox. Resized photos, renamed gallery photos.

--- scripts.js
+++ scripts.js
@@ -1,35 +1,49 @@
 // vanilla accordion - from codepen - I don't understand this yet...
 
-(function () {
-  var accordions, i;
+// (function () {
+//   var accordions, i;
   
-  // Make sure the browser supports what we are about to do.
-  if (!document.querySelectorAll || !document.body.classList) return;
+//   // Make sure the browser supports what we are about to do.
+//   if (!document.querySelectorAll || !document.body.classList) return;
   
-  // Using a function helps isolate each accordion from the others
-  function makeAccordion(accordion) {
-    var targets, currentTarget, i;
+//   // Using a function helps isolate each accordion from the others
+//   function makeAccordion(accordion) {
+//     var targets, currentTarget, i;
     
-    targets = accordion.querySelectorAll('.accordion > * > h1');
-    for(i = 0; i < targets.length; i++) {
-      targets[i].addEventListener('click', function () {
-        if (currentTarget)
-          currentTarget.classList.remove('expanded');
+//     targets = accordion.querySelectorAll('.accordion > * > h1');
+//     for(i = 0; i < targets.length; i++) {
+//       targets[i].addEventListener('click', function () {
+//         if (currentTarget)
+//           currentTarget.classList.remove('expanded');
         
-        currentTarget = this.parentNode;
-        currentTarget.classList.add('expanded');
-      }, false);
-    }
+//         currentTarget = this.parentNode;
+//         currentTarget.classList.add('expanded');
+//       }, false);
+//     }
 
-    accordion.classList.add('js');
-  }
+//     accordion.classList.add('js');
+//   }
 
-  // Find all the accordions to enable
-  accordions = document.querySelectorAll('.accordion');
+//   // Find all the accordions to enable
+//   accordions = document.querySelectorAll('.accordion');
   
-  // Array functions don't apply well to NodeLists
-  for(i = 0; i < accordions.length; i++) {
-    makeAccordion(accordions[i]);
-  }
+//   // Array functions don't apply well to NodeLists
+//   for(i = 0; i < accordions.length; i++) {
+//     makeAccordion(accordions[i]);
+//   }
   
-})();
+// })();
+
+// var largeImage = document.getElementById("large_image");
+// var thumbnails = document.getElementsByClassName("thumbnail");
+
+// for (var i = 0; i < thumbnails.length; i++) {
+//   var thumb = thumbnails[i];
+//   thumb.addEventListener("click", function(){
+//     var thumbHtml = this.innerHTML;
+//     largeImage.innerHTML=thumbHtml;
+//     this.setAttribute("id", "selected_thumb");
+//   });
+// }
+
+
